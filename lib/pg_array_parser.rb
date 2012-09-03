24a3217c90ebb41d600078b50e69b291866ad577
Dan McClain <git@danseaver.com>
2012-09-03 19:21:30 -0400

    Fixes regular ruby version

--- lib/pg_array_parser.rb
+++ lib/pg_array_parser.rb
@@ -1,7 +1,8 @@
 require File.expand_path('../pg_array_parser/version', __FILE__)
 require File.expand_path('../pg_array_parser', __FILE__)
-module PgArrayParser
-  if RUBY_PLATFORM =~ /java/
+
+if RUBY_PLATFORM =~ /java/
+  module PgArrayParser
     require 'jruby'
     require File.expand_path('../pg_array_parser.jar', __FILE__)
     require 'pgArrayParser/pg_array_parser_engine'
@@ -11,5 +12,7 @@ module PgArrayParser
       @parser.parse_pg_array(value)
     end
   end
+else
+  require 'pg_array_parser/pg_array_parser'
 end
 
